709a793cbfccd06a7991320cdbb372b31e019f82
alextina <castilloavilaa@gmail.com>
2023-08-25 12:42:16 -0500

    first commit

---
+++ api/api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const pokemonData = require('./data.json')
+
+app.get('/api/pokemon', (req, res) => {
+    res.json(pokemonData);
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`API running at: http://localhost:${PORT}`);
+    console.log(`API endpoint for Pokémon data: http://localhost:${PORT}/api/pokemon`);
+});
